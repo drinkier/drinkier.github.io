d31b0199f96cc2b4e18f41ee724c5230055a2f87
Kay Anar <github@anars.com>
2016-12-06 09:24:12 -0500

    Update

--- js/script.js
+++ js/script.js
@@ -1,82 +1,71 @@
-jQuery(document).ready(function($) {
+jQuery(document).ready(function ($) {
     var options = {
-        startDate: new Date(),
-        endDate: new Date("February 14, 2017 00:00:00")
+        startDate: new Date()
+        , endDate: new Date("February 14, 2017 00:00:00")
     };
-
     $('#container').page(options);
 });
-
-(function($) {
-
-    $.fn.page = function(opt) {
-
-        var obj = this,
-            /***   Default options   ***/
+(function ($) {
+    $.fn.page = function (opt) {
+        var obj = this
+            , /***   Default options   ***/
             defaults = {
-                startDate: new Date(),
-                endDate: new Date(),
-                titleDays: 'days',
-                titleHours: 'hours',
-                titleMinutes: 'minutes',
-                titleSeconds: 'seconds',
-                theme: 'custome'
-            },
-            options = $.extend(defaults, opt);
-
-        obj.init = function() {
-
-            // Activate count down
-            $("#countdown").dsCountDown(options);
-
-            // Activate tooltip for social media link
-            $(".sm-links a").aToolTip({
-                fixed: true,
-                inSpeed: 400,
-                xOffset: -107.5
-            });
-
-            $('#preloader').fadeOut('slow');
-
-            // Activate background
-            $('#background').hide();
-            var bg = $('#background img');
-            if (bg.size() == 1) {
-                var imageSrc = $('#background img').attr('src');
-                $.vegas({
-                    src: imageSrc,
-                    fade: 2000
-                });
+                startDate: new Date()
+                , endDate: new Date()
+                , titleDays: 'days'
+                , titleHours: 'hours'
+                , titleMinutes: 'minutes'
+                , titleSeconds: 'seconds'
+                , theme: 'custome'
             }
-        }
-
-        // Do preloader
-        obj.preloading = function() {
+            , options = $.extend(defaults, opt);
+        obj.init = function () {
+                // Activate count down
+                $("#countdown").dsCountDown(options);
+                // Activate tooltip for social media link
+                $(".sm-links a").aToolTip({
+                    fixed: true
+                    , inSpeed: 400
+                    , xOffset: -107.5
+                });
+                $('#preloader').fadeOut('slow');
+                // Activate background
+                $('#background').hide();
+                var bg = $('#background img');
+                if (bg.size() == 1) {
+                    var imageSrc = $('#background img').attr('src');
+                    $.vegas({
+                        src: imageSrc
+                        , fade: 2000
+                    });
+                }
+            }
+            // Do preloader
+        obj.preloading = function () {
             var preloader = $('<div id="preloader"><div class="loading"><div class="label">loading...</div></div></div>');
             obj.prepend(preloader);
             preloader.numberBg = $('#background img').size();
             if (preloader.numberBg) {
-                $('#background img').each(function(index) {
+                $('#background img').each(function (index) {
                     $('.loading', preloader).append('<span class="indicator-' + index + '"></span>');
                     var bg = $(this);
                     bg.itemIndex = index;
                     var img = new Image();
-                    img.onload = function() {
+                    img.onload = function () {
                         $('.indicator-' + bg.itemIndex).addClass('done');
                         if ($('.done', preloader).size() >= preloader.numberBg) {
-                            setTimeout(function() {
+                            setTimeout(function () {
                                 obj.init();
                             }, 500);
                         }
                     };
                     img.src = $(this).attr('src');
                 });
-            } else {
+            }
+            else {
                 obj.init();
             }
         }
         obj.preloading();
-
     }
-
 })(jQuery);
